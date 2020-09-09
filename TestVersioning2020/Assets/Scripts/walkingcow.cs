408c99511683be1a38ac4008ebf405b4093190b4
ETU\j.bourdin <j.bourdin@rubika-edu.com>
2020-09-09 15:22:36 +0200

    Ajout script vache

---
+++ TestVersioning2020/Assets/Scripts/walkingcow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingcow : MonoBehaviour
+{
+    public int vacheMagique;
+    public bool vacheMarche;
+    public int vitesseVache;
+
+    
+    // Start is called before the first frame update
+   
+    
+    void Start()
+    {
+        if (vacheMarche)
+        {
+            vacheMagique = 5
+            vitesseVache = 12
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
