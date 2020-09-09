e2007de5946c57a4fc2ab1c21f40b4e6a4258715
ETU\j.bourdin <j.bourdin@rubika-edu.com>
2020-09-09 16:12:02 +0200

    New animal scripts

--- TestVersioning2020/Assets/MoutonMoumoute.cs
+++ TestVersioning2020/Assets/MoutonMoumoute.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MoutonMoumoute : MonoBehaviour
 {
+    [SerializeField]
+    public bool nbreMouton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class MoutonMoumoute : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (nbreMouton)
+        {
+
+        }
     }
 }
