5649bcf1e4bdabaa9ca38874adfd255398e57eea
Alan Kilby <alan.kilby.cerquera@gmail.com>
2020-09-09 16:03:04 +0200

    Adding DuckityDuck game object
    DuckityDuck was added as a game object with an associated script named "duckingduck".

---
+++ TestVersioning2020/Assets/Scripts/duckingduck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duckingduck : MonoBehaviour
+{
+
+    public bool isTheDuckMad;
+    public int duckFood;
+    public int duckClothes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isTheDuckMad)
+        {
+            duckFood = 45;
+            duckClothes = duckFood + 55;
+        }
+        else
+        {
+            duckFood = 0;
+            duckClothes = 69;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
